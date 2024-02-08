2860283bf157924cc3b8dbf23c3734ea06816665
aelf-lu <yimeng.lu@aelf.io>
2024-02-08 19:10:31 +0800

    feat: add acs

--- .devcontainer/aelf-contract-templates/install.sh
+++ .devcontainer/aelf-contract-templates/install.sh
@@ -9,6 +9,7 @@ echo "The provided version is: $VERSION"
 ACS_DIR=$HOME/aelf-acs/Protobuf
 mkdir -p $ACS_DIR
 curl -O --output-dir $ACS_DIR https://raw.githubusercontent.com/AElfProject/AElf/dev/protobuf/acs12.proto
+echo "The path of acs is: $ACS_DIR"
 
 if [ $VERSION = "latest" ]; then
     dotnet new --install AElf.ContractTemplates
