956fec5ed9e96a90ee63fee812e07e2dfc39b843
aelf-lu <yimeng.lu@aelf.io>
2024-02-08 18:49:20 +0800

    feat: add acs

--- .devcontainer/aelf-contract-templates/install.sh
+++ .devcontainer/aelf-contract-templates/install.sh
@@ -6,6 +6,10 @@ echo "Activating feature 'aelf-contract-templates'"
 VERSION=${VERSION:-undefined}
 echo "The provided version is: $VERSION"
 
+ACS_DIR=$HOME/aelf-acs/Protobuf
+mkdir -p $ACS_DIR
+curl -O --output-dir $ACS_DIR https://raw.githubusercontent.com/AElfProject/AElf/dev/protobuf/acs12.proto
+
 if [ $VERSION = "latest" ]; then
     dotnet new --install AElf.ContractTemplates
 else
